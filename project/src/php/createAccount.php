4814d65afc9aa947f675b505934ae9094fa743b7
LucaEbne <l.ebner@students.htl-leonding.ac.at>
2026-04-21 16:22:29 +0200

    Update login page to link to create account page; add createAccount.php file with initial structure

--- project/src/php/createAccount.php
+++ project/src/php/createAccount.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -7,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Aegean Breeze</title>
     <link rel="icon" href="../../assets/img/Logo.png" type="image/png">
+    <script src="https://kit.fontawesome.com/3a03b4384b.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="../../src/styles/style.css">
     <link rel="stylesheet" href="../../src/styles/login.css">
     <script src="../../src/scripts/main.js" defer></script>
@@ -34,23 +34,44 @@
             <div class="login-card">
                 <div class="login-header">
                     <img src="../../assets/img/Logo.png" alt="Aegean Breeze Logo" class="login-logo">
-                    <h1>Welcome Back</h1>
-                    <p>Login to share your travel stories</p>
+                    <h1>Create your account</h1>
+                    <p>Join the community and share your island stories</p>
                 </div>
+
+                <div class="profile-upload-container">
+                    <div class="profile-image-wrapper">
+                        <img src="../../assets/img/default-profile-img.png" alt="Profile Preview"
+                            class="profile-preview-img">
+
+                        <div class="camera-badge">
+                            <i class="fa-regular fa-camera" style="color: rgb(255, 255, 255);"></i>
+                        </div>
+                    </div>
+                    <p class="upload-text">Click to upload a profile picture</p>
+                </div>
+
                 <form class="login-form">
                     <div class="form-group">
-                        <label for="email">Email</label>
-                        <input type="email" id="email" name="email" placeholder="your@email.com">
+                        <label for="name">Name</label>
+                        <input type="text" id="name" name="name" placeholder="Elena K.">
                     </div>
+
+                    <div class="form-group">
+                        <label for="email">Email</label>
+                        <input type="email" id="email" name="email" placeholder="you@example.com">
+                    </div>
+
                     <div class="form-group">
                         <label for="password">Password</label>
                         <input type="password" id="password" name="password" placeholder="Enter your password">
+                        <span class="input-hint">Must be at least 6 characters</span>
                     </div>
-                    <button type="button" class="btn btn-login">Login</button>
+
+                    <button type="button" class="btn-login">Create account</button>
                 </form>
 
                 <div class="login-footer">
-                    <p>Don't have an account? <a href="../../src/php/createAccount.php">Create an account</a></p>
+                    <p>Already have an account? <a href="#">Log in</a></p>
                 </div>
             </div>
         </div>
